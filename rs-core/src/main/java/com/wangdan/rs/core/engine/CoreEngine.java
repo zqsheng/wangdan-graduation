da4abfc3fe162692f95c88ec7fe106836f45e8ed
张启生 <1160265991@qq.com>
2019-12-16 16:55:25 +0800

    提交

--- rs-core/src/main/java/com/wangdan/rs/core/engine/CoreEngine.java
+++ rs-core/src/main/java/com/wangdan/rs/core/engine/CoreEngine.java
@@ -1,7 +1,15 @@
 package com.wangdan.rs.core.engine;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class CoreEngine {
 
     public static void main(String[] args) {
+      SparkConf sparkConf = new SparkConf().setAppName("spark main").setMaster("local");
+      JavaSparkContext context = new JavaSparkContext(sparkConf);
+      String url = "jdbc:mysql://localhost:3306/website?user=root;password=gxst123456";
+//      new JavaS
+//      JavaSpark
     }
 }
